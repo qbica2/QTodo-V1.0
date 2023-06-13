1603885860c5c54c62e82459868c6e92e1c4fdb8
Kubilay Akdemir <kubidik27@gmail.com>
2023-06-14 01:00:48 +0300

    Initial Commit

---
+++ QTodo-v1.0/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  QTodo-v1.0
+//
+//  Created by Mehmet Kubilay Akdemir on 14.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QTodo-v1.0/QTodo_v1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  QTodo_v1_0App.swift
+//  QTodo-v1.0
+//
+//  Created by Mehmet Kubilay Akdemir on 14.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct QTodo_v1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
